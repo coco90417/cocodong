e92db50a282967b9cffac0d9ba561ddb09345e2b
coco <coco90417@users.noreply.github.com>
2018-07-12 11:19:47 -0400

    Update Van_Gogh_Or_Monet.R

--- Van_Gogh_Or_Monet.R
+++ Van_Gogh_Or_Monet.R
@@ -1,6 +1,6 @@
 ## to load monet, run this
 ## library(devtools)
-## source_url("https://raw.githubusercontent.com/coco90417/cocodong/master/Load_cars.R")
+## source_url("https://raw.githubusercontent.com/coco90417/cocodong/master/Van_Gogh_Or_Monet.R")
 ## monet = load_monet()
 
 load_monet = function() {
